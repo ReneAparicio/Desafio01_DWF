cbf65df6bb2215bd4db8fa62322885c3564e45a7
Ghost-guss1622 <87097371+Ghost-guss1622@users.noreply.github.com>
2025-08-03 00:26:16 -0600

    Add files via upload

---
+++ Desafio01_DWF-main/bookapi/src/main/java/com/letrasvivas/bookapi/controller/BookController.java
@@ -0,0 +1,51 @@
+package com.letrasvivas.bookapi.controller;
+
+import com.letrasvivas.bookapi.model.Book;
+import com.letrasvivas.bookapi.service.BookService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+  private final BookService service;
+  public BookController(BookService service) {
+    this.service = service;
+  }
+
+  @GetMapping
+  public List<Book> all() {
+    return service.getAllBooks();
+  }
+
+  @GetMapping("/{id}")
+  public Optional<Book> one(@PathVariable Long id) {
+    return service.getBookById(id);
+  }
+
+  @PostMapping
+  public Book create(@RequestBody Book book) {
+    return service.createBook(book);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    try {
+      service.deleteBook(id);
+      return ResponseEntity.noContent().build();
+    } catch (EntityNotFoundException e) {
+      return ResponseEntity.notFound().build(); // En caso que no exista el id
+    }
+  }
+
+  @GetMapping("/search")
+  public List<Book> search(@RequestParam String title) {
+    return service.searchBooks(title);
+  }
+}
+
+
